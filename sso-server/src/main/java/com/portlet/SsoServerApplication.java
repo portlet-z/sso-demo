ffc30a8f33d9a0ccf0400b55be225a76071cc0ee
张新征 <programer.email@gmail.com>
2018-12-24 22:39:32 +0800

    init project

---
+++ sso-server/src/main/java/com/portlet/SsoServerApplication.java
@@ -0,0 +1,15 @@
+package com.portlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangxinzheng
+ * @date 2018-12-24
+ */
+@SpringBootApplication
+public class SsoServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApplication.class, args);
+    }
+}
